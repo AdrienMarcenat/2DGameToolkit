baa3cc4ac86281a7c6aaabddd442cbb5a157a376
Adrien Marcenat <adrien.marcenat@telecom-paristech.fr>
2018-08-05 18:18:17 +0200

    add unittest

---
+++ 2DGameToolKitTest/UniqueProxyTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2DGameToolKitTest
+{
+    [TestClass]
+    public class UniqueProxyTest
+    {
+        class DummyClass
+        { }
+
+        class DummyClassProxy : UniqueProxy<DummyClass>
+        { }
+
+        [TestCleanup]
+        public void TestCleanup ()
+        {
+            if (DummyClassProxy.IsValid ())
+            {
+                DummyClassProxy.Close ();
+            }
+        }
+
+        [TestMethod]
+        public void TestOpen ()
+        {
+            DummyClass dummyClass = new DummyClass ();
+            DummyClassProxy.Open (dummyClass);
+            Assert.IsTrue (DummyClassProxy.IsValid ());
+            Assert.AreEqual (dummyClass, DummyClassProxy.Get ());
+        }
+
+        [TestMethod]
+        public void TestCloseAfterOpen ()
+        {
+            DummyClass dummyClass = new DummyClass ();
+            DummyClassProxy.Open (dummyClass);
+
+            DummyClassProxy.Close ();
+            Assert.ThrowsException<NUnit.Framework.AssertionException> (delegate { DummyClassProxy.Get (); });
+        }
+
+        [TestMethod]
+        public void TestCloseBeforeOpen ()
+        {
+            Assert.ThrowsException<NUnit.Framework.AssertionException> (delegate { DummyClassProxy.Close (); });
+        }
+
+        [TestMethod]
+        public void TestDoubleOpen ()
+        {
+            DummyClass dummyClass = new DummyClass ();
+            DummyClassProxy.Open (dummyClass);
+            Assert.AreEqual (dummyClass, DummyClassProxy.Get ());
+            Assert.ThrowsException<NUnit.Framework.AssertionException> (delegate { DummyClassProxy.Open (dummyClass); });
+        }
+    }
+}
